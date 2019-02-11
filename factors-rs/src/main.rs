b479abbe4b96acb6c6701ab69c095e8969d26be1
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-02-11 02:55:20 -0800

    Commit all. SIGNIFICANT improvements. Yay!

--- factors-rs/src/main.rs
+++ factors-rs/src/main.rs
@@ -1,9 +1,13 @@
+// (c) Fernando Gonzalez-Morales, 2019
+// That's right.
 use std::env;
 use std::fs;
 use std::cmp::{max, min};
 use rand::Rng;
-use num_integer::Roots;
-use primal;
+use num::BigUint;
+use num::cast::ToPrimitive;
+use num::traits::identities::One;
+//use primal;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,68 +17,41 @@ fn main() {
         .filter_map(|line| line.trim().parse().ok())
         .collect();
     for n in nums {
-        let d = if (n % 2) == 0 { 2 }
-           else if (n % 3) == 0 { 3 }
-           else if (n % 5) == 0 { 5 }
-           else if (n % 7) == 0 { 7 }
-           else {pollard_rho(n)};
+        let d = pollard_rho(n);
         println!("{}={}*{}", n, n / d, d);
     }
 }
-/*    let biggest:  = *nums.iter().max().unwrap();
-    let sieve = primal::Primes::new(biggest.sqrt());
-    for n in nums {
-        for d in sieve {
-            if n % d == 0 {
-                println!("{}={}*{}", n, n / d, d);
-                break;
-            }
-        }
-    }
-}*/
 
-fn _pollard_rho(num: u128) -> u128 {
-    if num == 1 {return num;}
-    if num % 2 == 0 {return 2;}
-    let mut x = rand::thread_rng().gen_range(2, num);
-    let mut y = x;
-    let c = rand::thread_rng().gen_range(1, num);
-    let f = |i| (_mod_pow(i, 2, num) + c + num) % num;
-    let mut d = 1;
+fn pollard_rho(num: u128) -> u128 {
+    if num == 1 { return num; }
+    if num % 2 == 0 { return 2; }
+    let k = rand::thread_rng().gen_range(2, num);
+    let (mut d, mut x, mut y) = (1, k, k);
+    let f = |i| (mod_pow(i, 2, num) + k + num) % num;
     while d == 1 {
         x = f(x);
         y = f(f(y));
-        let diff = if x > y { x - y } else { y - x };
+        let diff = if y > x { y - x } else { x - y };
         d = stein_gcd(diff, num);
         if d == num {
-            break;
+            return 1;
         }
     }
     d
 }
 
-fn pollard_rho(num: u128) -> u128 {
-    let k = rand::thread_rng().gen_range(2, num);
-    let (mut d, mut n, mut p) = (1, k, k);
-    let f = |x| (x * x + k) % num;
-    while d == 1 {
-        n = f(n);
-        p = f(f(p));
-        let diff = if p > n { p - n } else { n - p};
-        d = stein_gcd(diff, num);
-        if d == num { break; }
+fn mod_pow(a: u128, b: u128, m: u128) -> u128 {
+    let mut r = BigUint::one();
+    let mut x = BigUint::from(a);
+    let mut y = b;
+    while y > 0 {
+        if y & 1 == 1 { r = (r * &x) % m; }
+        y = y >> 1;
+        x = (&x * &x) % m;
     }
-    d
-}
-
-fn _mod_pow(a: u128, b: u128, m: u128) -> u128 {
-    let mut r = 1;
-    while b > 0 {
-        if b & 1 == 1 { r = (r * a) % m; }
-        let mut b = b >> 1;
-        let mut a = (a * a) % m;
-    }
-    r
+    use std::u128;
+    let mask = BigUint::from(u128::MAX);
+    (r & mask).to_u128().unwrap()
 }
 
 fn stein_gcd(m: u128, n: u128) -> u128 {
@@ -89,3 +66,34 @@ fn stein_gcd(m: u128, n: u128) -> u128 {
         _                                   => unreachable!(),
     }
 }
+
+// SCRATCH PAD :D
+/*  let biggest = *nums.iter().max().unwrap();
+    let sieve = primal::Primes::new(biggest);
+    for n in nums {
+        for d in sieve {
+            if n % d == 0 {
+                println!("{}={}*{}", n, n / d, d);
+                break;
+}}}}
+
+fn _pollard_rho(num: u128) -> u128 {
+    if num == 1 { return num; }
+    if num % 2 == 0 { return 2; }
+    let mut x = rand::thread_rng().gen_range(2, num);
+    let mut y = x;
+    let c = rand::thread_rng().gen_range(1, num);
+    let f = |i| (mod_pow(i, 2, num) + c + num) % num;
+    let mut d = 1;
+    while d == 1 {
+        x = f(x);
+        y = f(f(y));
+        let diff = if x > y { x - y } else { y - x };
+        d = stein_gcd(diff, num);
+        if d == num {
+            return 1;
+        }
+    }
+    d
+}
+*/
